c53fcab3ea10ab663eeeda21f7652529a78b0455
Kaili Kameoka <kaili.kameoka@gmail.com>
2023-04-27 09:22:43 -1000

    set up 7th chord array.

--- src/chords/7Chords.ts
+++ src/chords/7Chords.ts
@@ -1,32 +1,40 @@
-import cDimFile from '../assets/c-dim.mp3';
-import cDimHintFile from '../assets/c-dim-hint.mp3';
-import cMinFile from '../assets/c-min.mp3';
-import cMinHintFile from '../assets/c-min-hint.mp3';
-import cMajFile from '../assets/c-maj.mp3';
-import cMajHintFile from '../assets/c-maj-hint.mp3';
-import cAugFile from '../assets/c-aug.mp3';
-import cAugHintFile from '../assets/c-aug-hint.mp3';
+import c7DimFile from '../assets/c7-dim.mp3';
+import c7DimHintFile from '../assets/c7-dim-hint.mp3';
+import c7HalfDimFile from '../assets/c7-half-dim-hint.mp3';
+import c7HalfDimHintFile from '../assets/c7-half-dim-hint.mp3';
+import c7MinFile from '../assets/c7-min.mp3';
+import c7MinHintFile from '../assets/c7-min-hint.mp3';
+import c7MajMinFile from '../assets/c7-maj-min.mp3';
+import c7MajMinHintFile from '../assets/c7-maj-min-hint.mp3';
+import c7MajFile from '../assets/c7-maj.mp3';
+import c7MajHintFile from '../assets/c7-min-hint.mp3';
+
 import { Chord, chordTypes } from "./types";
 
-export const triadChordData: Chord[] = [
+export const sevenChordData: Chord[] = [
 	{
-		mainSrc: cDimFile,
-		hintSrc: cDimHintFile,
-		type: chordTypes.dim
+		mainSrc: c7DimFile,
+		hintSrc: c7DimHintFile,
+		type: chordTypes.dim7
 	},
 	{
-		mainSrc: cMinFile,
-		hintSrc: cMinHintFile,
-		type: chordTypes.min
+		mainSrc: c7HalfDimFile,
+		hintSrc: c7HalfDimHintFile,
+		type: chordTypes.halfDim7
 	},
 	{
-		mainSrc: cMajFile,
-		hintSrc: cMajHintFile,
-		type: chordTypes.maj
+		mainSrc: c7MinFile,
+		hintSrc: c7MinHintFile,
+		type: chordTypes.min7
 	},
 	{
-		mainSrc: cAugFile,
-		hintSrc: cAugHintFile,
-		type: chordTypes.aug
+		mainSrc: c7MajMinFile,
+		hintSrc: c7MajMinHintFile,
+		type: chordTypes.majMin7
+	},
+	{
+		mainSrc: c7MajFile,
+		hintSrc: c7MajHintFile,
+		type: chordTypes.maj7
 	}
 ]
